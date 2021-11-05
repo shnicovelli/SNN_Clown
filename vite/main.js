9bf71bbc04961f729a4b8bbbf694ae0cdc7bb1f7
Naoufal Bensaiad <70409892+NaoufalBen@users.noreply.github.com>
2021-11-05 13:49:14 -0400

    font et js reparations

--- vite/main.js
+++ vite/main.js
@@ -2,7 +2,4 @@ import './style.scss'
 import './swipper'
 import './gsap'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
