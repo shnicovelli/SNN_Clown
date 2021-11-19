23cd3ea84c0777d86a1040d3f3418f0443b33bfd
Samuel Poulin <70410165+SamuelPoulin1@users.noreply.github.com>
2021-11-19 15:23:48 -0500

    js swiper et bloc don

--- wordpress/snn/wp-content/themes/theme-de-base/main.js
+++ wordpress/snn/wp-content/themes/theme-de-base/main.js
@@ -1,1 +1,39 @@
-/* Wow, tellement un beau spot pour écrire du JS */
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+  
+  
+  
+  const btn = document.querySelector('.btn ')
+  
+  
+  btn.addEventListener('click', () => {
+    gsap.timeline()
+      .fromTo('.ballon1', 
+      { opacity: '0%' , skewX : '0%', y : '-5%'}, 
+      { opacity: '100%',  skewX : '5%',  y : '-80%' ,duration:1.5,   }
+    )
+    .fromTo('.ballon2', 
+      { opacity: '0%' , skewX : '0%', y : '-5%'}, 
+      { opacity: '100%',  skewX : '-10%',  y : '-80%' ,duration:1.5,   }
+    )
+    .fromTo('.ballon3', 
+      { opacity: '0%' , skewX : '0%', y : '-5%'}, 
+      { opacity: '100%',  skewX : '10%',  y : '-80%' ,duration:1.5,   }
+    );
+  });
+  
+  
