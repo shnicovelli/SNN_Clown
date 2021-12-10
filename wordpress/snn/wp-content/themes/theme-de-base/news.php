724e35c4f85e9ed4aa20ca548b7a468ca59a88a4
Naoufal Bensaiad <70409892+NaoufalBen@users.noreply.github.com>
2021-12-09 22:23:03 -0500

    news.php

--- wordpress/snn/wp-content/themes/theme-de-base/news.php
+++ wordpress/snn/wp-content/themes/theme-de-base/news.php
@@ -11,57 +11,9 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
 	while ( have_posts() ) : the_post(); 
 ?>
 
-<p class="titre-heros-detail-nouvelle">Haïti « On est dans une crise humanitaire »</p>
-<img src="https://raw.githubusercontent.com/shnicovelli/SNN_Clown/main/sources/medias/haiti.jpg" alt="" class="haiti">
-<p class="texte-heros-detail-nouvelle">1 mars 2020 | Haïti</p>
-<p class="texte1">Des affrontements mortels entre policiers et militaires. Des millions d’habitants confrontés à l’insécurité alimentaire. Un
-pouvoir d’achat en déclin. Haïti s’enfonce dans la crise.</p>
-<p class="titre1">ÉTIENNE CÔTÉ PALUCK<br>COLLABORATION SPÉCIALE</p>
-<p class="texte2">PORT-AU-PRINCE — Des « airs de guerre civile ». C’est ainsi que la presse haïtienne a décrit les récents affrontements
-qui ont opposé policiers et militaires haïtiens au cœur de la capitale, forçant l’annulation du traditionnel carnaval. Alors
-que Haïti traverse une grave crise humanitaire, beaucoup n’ont plus le cœur à la fête.<br><br>
-« Je ne sais pas qui a tiré le premier, ça s’est passé tellement vite. J’ai dû ramper quand j’ai voulu partir, avec toutes ces
-balles qui sifflaient », relate un marchand de crème glacée de 26 ans, Luckens. L’homme qui gère un kiosque à la grande
-place de Port-au-Prince a préféré taire son véritable nom, craignant les représailles.<br><br>
-Dimanche dernier, des policiers étaient descendus dans les rues pour réclamer le droit de se syndiquer. Après 20 mois
-successifs de manifestations, de blocus routiers et, parfois, d’émeutes contre le gouvernement, les agents se disent à
-bout de souffle et affirment ne plus réussir à boucler les fins de mois avec leur maigre salaire. La marche a toutefois
-rapidement tourné à l’affrontement avec les militaires.<br><br>
-« Le gouvernement ne comprend pas quand on fait une simple manifestation [pacifique] », se désole Luckens.
-La Montréalaise Katel Le Fustec s’apprêtait à participer au premier jour de carnaval à Port-au-Prince avec sa troupe de
-Clowns Sans Frontières quand les affrontements ont éclaté. « On a entendu des tirs », se souvient-elle, de retour sur
-place avec La Presse.<br><br>
-Deux jeunes acrobates ont alors décidé d’aller se renseigner. « Ils sont revenus pétrifiés en disant : “Il n’y a pas de
-carnaval, on annule tout”. »</p>
-<p class="titre2">CARNAVAL ANNULÉ</p>
-<p class="texte3">Les échanges de tirs entre policiers et militaires ont duré au moins six heures, faisant deux morts et une dizaine de
-blessés. Devant ces dérapages, le gouvernement a officiellement annulé en soirée les célébrations des trois jours gras,
-pourtant une véritable institution pour le pays.<br><br>
-La tenue du carnaval était plutôt jugée indécente par les manifestants, alors que le pays traverse l’une des plus graves
-crises économiques de son histoire récente. Le bureau de coordination des Nations unies des affaires humanitaires
-(OCHA) évalue que 3,7 millions d’Haïtiens sont en situation d’insécurité alimentaire cette année. L’inflation annuelle est
-autour de 20 %, réduisant inexorablement le pouvoir d’achat des Haïtiens.<br><br>
-« Je suis sérieusement découragé du pays. Tous les efforts que tu fais ne servent à rien. Tu travailles, mais tu n’arrives
-pas à économiser pour l’avenir ni à répondre à tous tes besoins de base. » — Luckens, 26 ans, marchand de crème
-glacée.<br><br>
-« Pendant longtemps, je me tenais droit, fier d’être Haïtien », se souvient Luckens, devant son kiosque sur lequel est
-peint un immense cône de crème glacée multicolore. Sa voix tremble d’émotion et ses yeux se remplissent d’eau. «
-Aujourd’hui, je ne peux plus dire ça. »<br><br>
-Ce constat, beaucoup le partagent. « C’est un pays dans lequel les problèmes qui devraient être gérés ne le sont pas »,
-déplore Michel Joseph, codirecteur de l’information et présentateur du bulletin de 19 h, à Radio Caraïbes. Les bureaux
-de la plus importante radio privée au pays ont été la cible d’une poignée d’émeutiers, dimanche, qui ont incendié une
-génératrice, deux véhicules de diffusion et quatre autres voitures de particuliers garées devant la station.<br><br>
-Depuis l’attaque, condamnée par l’Association des journalistes haïtiens, la station ne diffuse plus d’émissions originales,
-le temps de se remettre des dégâts.<br><br>
-« On est dans une crise humanitaire », rappelle à La Presse le reporter et animateur de 32 ans à quelques pas des
-carcasses de voiture calcinées. « Les gens ont faim. »
-Katel Le Fustec, qui doit rentrer à Montréal dans quelques jours, comprend le découragement, même si elle refuse
-d’abandonner. Depuis 2002, l’organisation Clowns Sans Frontières qu’elle dirige accompagne des jeunes de milieux
-défavorisés haïtiens dans les arts du cirque. « J’y crois, parce que je travaille avec des jeunes engagés de la relève. C’est
-ça qui garde mon espoir en vie. »<br><br>
-Ce texte provenant de La Presse+ est une copie en format web. Consultez-le gratuitement:
-<a href="https://plus.lapresse.ca/screens/8e43315f-0f4c-41c9-a65c-f497eb5eea1f__7C___0.html" class="lien-lapresse">https://plus.lapresse.ca/screens/8e43315f-0f4c-41c9-a65c-f497eb5eea1f__7C___0.html</a><br><br><br>
-Crédit photo : Katel Le Fustec</p>
+<p class="titre-heros-detail-nouvelle"><?php the_title()?></p>
+<?php the_post_thumbnail()?>
+<?php the_content()?>
 
 <div class="bloc-partage">
   <div class="reseaux-bloc-partage">
